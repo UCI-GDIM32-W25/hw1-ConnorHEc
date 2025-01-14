5232a04b9bc3a2a5739ac008b83d0737b321fb7b
ConnorHEc <hcjspeaker@gmail.com>
2025-01-13 18:14:35 -0800

    Test1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,42 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        // Initialize seeds
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+
+        // Update the UI at the start
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        HandleMovement();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
     }
+     private void HandleMovement()
+    {
+        float moveX = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
 
+        _playerTransform.Translate(moveX, moveY, 0);
+    }
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft > 0)
+        {
+            // Create a plant at the player's position
+            Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+
+            // Update seed counts
+            _numSeedsLeft--;
+            _numSeedsPlanted++;
+
+            // Update the UI
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+        }
     }
 }
